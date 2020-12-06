c7a63399491d9ace34a4fef577e13bb075bba1f8
BuildTools <cubemaster118@gmail.com>
2020-12-06 18:35:20 -0500

    Testing file path issues
    Uploading to my branch to see if file paths make it difficult for us to collaborate

---
+++ Simon.srcs/sources_1/new/testfile.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/06/2020 06:12:06 PM
+// Design Name: 
+// Module Name: testfile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testfile(
+
+    );
+endmodule
